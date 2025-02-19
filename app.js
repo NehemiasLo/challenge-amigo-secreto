cf66d57008ed8c3db649dced86e04b7dde2ed8ff
Nehemias <yamithlopez1@gmail.com>
2025-02-19 18:21:07 -0500

    funcion agragar amigos

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = []; // Se crea un array vacío para almacenar los amigos
+
+function refrescarBotonSortear() {
+    let botonSortear = document.getElementById('button-draw'); // Se obtiene el botón de sortear
+    if (listaAmigos.length < 2) { // Si la cantidad de amigos es menor a 2
+        botonSortear.disabled = true; // Se deshabilita el botón de sortear
+    } else { // Si la cantidad de amigos es mayor o igual a 2
+        botonSortear.disabled = false; // Se habilita el botón de sortear
+    }
+}
+
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById('amigo').value.trim(); // Se obtiene el valor del input
+
+    let regex = /^[a-zA-Z]+$/; // Se crea una expresión regular para validar que el nombre solo contenga letras
+    if (nombreAmigo=='') { // Se valida que el input no esté vacío") {
+        alert('Debes ingresar un nombre');
+        return;
+    }
+    if (!regex.test(nombreAmigo)) { // Se valida que el nombre solo contenga letras
+        alert('El nombre solo debe contener letras y espacios');
+        return;
+    }
+    let existe = listaAmigos.find(amigo => amigo.toLowerCase() === nombreAmigo.toLowerCase()); // Se busca el nombre en el array
+    if (existe) { // Si el nombre ya existe en el array se muestra un mensaje de error
+        alert('El amigo ya existe');
+        return;
+    }
+
+    listaAmigos.push(nombreAmigo); // Se agrega el nombre al array
+    document.getElementById('amigo').value = ''; // Se limpia el input
+    document.getElementById('amigo').blur(); // Se quita el foco del input
+    actualizarListaAmigos(); // Se llama a la función que actualiza la lista de amigos
+    refrescarBotonSortear(); // Se llama a la función que refresca el estado del botón de sortear
+
+}
