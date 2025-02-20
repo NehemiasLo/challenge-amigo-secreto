8dd6dcf862c822a383ccf72a2fbfe292677c067e
Nehemias <yamithlopez1@gmail.com>
2025-02-19 23:34:52 -0500

    Actualizar lista

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ let asignaciones = {}; // Se crea un objeto vacío para almacenar las asignacion
 
 
 function refrescarBotonSortear() {
-    let botonSortear = document.getElementById('button-draw'); // Se obtiene el botón de sortear
-    botonSortear.disabled = listaAmigos.length < 2; // Se deshabilita el botón si la cantidad de amigos es menor a 2
+    let botonSortear = document.getElementById('button-sorteo'); 
+    //botonSortear.disabled = listaAmigos.length < 2; // Se deshabilita el botón si la cantidad de amigos es menor a 2
     
-    /*if (listaAmigos.length < 2) { // Si la cantidad de amigos es menor a 2
+    if (listaAmigos.length < 2) { // Si la cantidad de amigos es menor a 2
         botonSortear.disabled = true; // Se deshabilita el botón de sortear
     } else { // Si la cantidad de amigos es mayor o igual a 2
         botonSortear.disabled = false; // Se habilita el botón de sortear
-    }*/
+    }
 }
 
 function agregarAmigo() {
@@ -43,7 +43,7 @@ function agregarAmigo() {
 }
 
 function sortearAmigo() {
-    if (listaAmigos.length < 2) { // Si la cantidad de amigos es menor a 2
+    if (listaAmigos.length < 2) { 
         alert('Debes ingresar al menos dos amigos'); // Se muestra un mensaje de error
         return;
     }
@@ -55,16 +55,16 @@ function sortearAmigo() {
         let posiblesAmigos = amigosDisponibles.filter(a => a.toLowerCase() !== amigo.toLowerCase()); // Se filtran los amigos disponibles
 
         if (posiblesAmigos.length === 0) { // Si no hay amigos disponibles
-            alert('No se puede realizar el sorteo. Intenta nuevamente'); // Se muestra un mensaje de error
+            alert('No se puede realizar el sorteo. Intente nuevamente'); // Se muestra un mensaje de error
             return;
         }
-        
-        let indiceAleatorio = Math.floor(Math.random() * posiblesAmigos.length); // Se obtiene un índice aleatorio
+             
+        let indiceAleatorio = Math.floor(Math.random() * posiblesAmigos.length); // Se obtiene un índice(amigo) aleatorio 
         let amigoSecreto = posiblesAmigos[indiceAleatorio]; // Se obtiene el amigo secreto
+        
         asignacionesTemp[amigo] = amigoSecreto; // Se asigna el amigo secreto al amigo actual
-
         amigosDisponibles = amigosDisponibles.filter(a => a !== amigoSecreto); // Se filtran los amigos disponibles
-    
+
     }
 
     asignaciones = asignacionesTemp; // Se asignan las asignaciones al objeto global
@@ -73,7 +73,8 @@ function sortearAmigo() {
 }
 
 function mostrarResultados() {
-    let resultadosHTML = ""; // Se crea una variable para almacenar el HTML de los resultados
+    let resultadosHTML = ""; // Se crea una variable para almacenar el HTML de los resultados    
+
     for (let amigo in asignaciones) { // Se recorren las asignaciones
        
         resultadosHTML += `<li class="result-item"> <span class="result-name">${amigo}</span> 
@@ -85,11 +86,25 @@ function mostrarResultados() {
     resultadoLista.classList.add('mostar'); // Se agrega la clase para mostrar los resultados
 
     listaAmigos = []; // Se reinicia el array de amigos
-    document.getElementById('lista-amigos').innerHTML = ''; // Se limpia la lista de amigos
+    document.getElementById('listaAmigos').innerHTML = ''; // Se limpia la lista de amigos
     refrescarBotonSortear(); // Se llama a la función que refresca el estado del botón de sortear
 
     setTimeout(() => {
-        resultadoLista.scrollIntoView({ behavior: 'smooth' }); // Se hace scroll para mostrar los resultados
+        alert('Fin del sorteo'); // Se muestra un mensaje de alerta
+        location.reload(); // Se recarga la página
     }, 100);
 
+}
+
+function actualizarListaAmigos() {
+    let ul = document.getElementById('listaAmigos'); // Se obtiene el elemento donde se mostrarán los amigos
+    ul.innerHTML = ''; // Se limpia el contenido del elemento
+
+    for (let amigo of listaAmigos) { // Se recorre el array de amigos
+        let li = document.createElement('li'); // Se crea un elemento li
+        li.textContent = amigo; // Se asigna el nombre del amigo al li
+        ul.appendChild(li); 
+    }
+
+    
 }
